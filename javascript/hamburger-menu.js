a075f57efbc653681be19e1bdf53c129f3098f3f
tamoroso <amorosothomas@orange.fr>
2021-07-19 13:14:33 +0200

    Adding responsive design

--- javascript/hamburger-menu.js
+++ javascript/hamburger-menu.js
@@ -3,12 +3,15 @@ const icon = document.getElementById("nav-menu-responsive__hamburger");
 function showResponsiveMenu() {
     var menu = document.getElementById("nav-menu-responsive");
     var heroBanner = document.getElementById("hero-banner");
+    var iconPath = document.getElementById("icon-path");
     if(menu.className === "nav-menu-responsive") {
         menu.classList.add("nav-menu-responsive--open");
         heroBanner.style.overflowY = "hidden";
+        iconPath.style.fill = "hsla(0, 100%, 100%, 0.64)";
     } else {
         menu.classList = "nav-menu-responsive";
         heroBanner.style.overflowY = "";
+        iconPath.style.fill = "";
     }
 }
 
